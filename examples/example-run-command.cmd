1a25b5af56699a757347f884209e6b8edb42453e
Aaron Lahman <aaronla@microsoft.com>
2021-05-28 12:22:06 -0700

    Working more on the embed example

--- examples/example-run-command.cmd
+++ examples/example-run-command.cmd
@@ -1,12 +1,22 @@
 <# :: 
 @echo off
+::
+:: Self contained script
+
 setlocal
-set _args=%*
-::set _args=%_args:"="""%
-::set _args=%_args:`=``%
-::set _args=%_args:$=`$%
-powershell -noni -nop -ex Bypass -Command "& (iex ('{ ' + (Get-Content -Raw '%~f0') + '}')) %_args% "
+:argsloop
+if "%~1" == "" goto :argsdone
+set _arg=%~1
+set _arg=%_arg:"="""%
+set _arg=%_arg:`=``%
+set _arg=%_arg:$=`$%
+set _args=%_args% "%_arg%"
+shift
+goto :argsloop
+:argsdone
+echo %_args%
 ::powershell -noni -nop -ex Bypass -Command "& (iex ('{ ' + (Get-Content -Raw '%~f0') + '}')) %_args% "
+::powershell -noni -nop -ex Bypass -Command "& (iex """{ $(Get-Content -Raw '%~f0') }""") %_args% "
 ::powershell -noni -nop -ex Bypass -Command "echo (iex ('{ ' + (Get-Content -Raw '%~f0') + '}'))"
 goto :eof
 #>
